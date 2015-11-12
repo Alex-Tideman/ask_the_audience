35a7eadb6599b597a96902400b7caca05825a3b4
Alex Tideman <alex.tideman@gmail.com>
2015-11-12 11:01:12 -0700

    Finished unpretty version

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const express = require('express');
-const socketIo = require('socket.io');
+const _ = require('lodash');
 const app = express();
 
 app.use(express.static('public'));
@@ -9,24 +9,63 @@ app.get('/', function (req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var server = http.createServer(app)
-    .listen(port, function () {
+const server = http.createServer(app)
+    .listen(port, function(){
         console.log('Listening on port ' + port + '.');
     });
 
+const socketIo = require('socket.io');
 const io = socketIo(server);
 
 
-io.on('connection', function (socket) {
+var votes = {};
+
+io.on('connection', function(socket){
     console.log('A user has connected.', io.engine.clientsCount);
+
+    socket.emit('statusMessage', 'You have connected.');
+
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+    io.sockets.emit('voteCount', countVotes(votes));
+
+    socket.on('message', function (channel, message) {
+        if (channel === 'voteCast') {
+            votes[socket.id] = message;
+            io.sockets.emit('voteCount', countVotes(votes));
+            socket.emit('voteMessage', message);
+        }
+    });
+
+    socket.on('disconnect', function(){
+        console.log('A user has disconnected.', io.engine.clientsCount);
+        delete votes[socket.id];
+        socket.emit('voteCount', countVotes(votes));
+        io.sockets.emit('userConnection', io.engine.clientsCount);
+    });
+
 });
 
 
+function countVotes(votes) {
+    var voteCount = {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+    };
+    for (vote in votes) {
+        voteCount[votes[vote]]++
+    }
 
+    var results = "";
 
+    _.forOwn(voteCount, function(count,vote) {
+        results += vote + ":" + count + "  ";
+    });
 
-
+    return results;
+}
 
 module.exports = server;
